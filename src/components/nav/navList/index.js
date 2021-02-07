8fd2748f67a63fe83586a9d6b89b61359ea5ae08
Moira <moira.walker@live.nl>
2021-02-07 20:34:17 +0100

    added admin state to link

--- src/components/nav/navList/index.js
+++ src/components/nav/navList/index.js
@@ -2,14 +2,15 @@ import './index.scss';
 import { NavLink } from "react-router-dom";
 import { BurgerBarContext } from "../../../context/burgerBarContextProvider";
 import { useContext } from 'react';
-
+import { AuthContext } from "../../../context/authContextProvider";
 
 export const NavList = () => {
+    const {getAdmin} = useContext(AuthContext);
     const { open, setOpen } = useContext(BurgerBarContext);
 
     return(
         <ul className={open ? 'nav-list nav-list--open' : 'nav-list nav-list--closed'} >
-            <li><NavLink to="/admin" exact activeClassName="nav-link--active" className="nav-link nav-link__padding nav-link--is-authorised">Admin</NavLink></li>
+            <li>{ getAdmin() &&  <NavLink to="/admin" exact activeClassName="nav-link--active" className="nav-link nav-link__padding nav-link--is-authorised">Admin</NavLink>}</li>
             <li><NavLink to="/" exact activeClassName="nav-link--active" className="nav-link nav-link__padding">Projects</NavLink></li>
             <li><NavLink to="/account" exact activeClassName="nav-link--active" className="nav-link nav-link__padding">Moira</NavLink></li>
             <li><NavLink to="/account" exact activeClassName="nav-link--active" className="nav-link nav-link__logout">Logout</NavLink></li>
--- src/routing/privateRouteAdmin/index.js
+++ src/routing/privateRouteAdmin/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import {AuthContext, useAuthState} from '../../context/authContextProvider'
 import {Redirect, Route} from 'react-router-dom';
+import {AuthContext, useAuthState} from '../../context/authContextProvider'
 import {useContext} from 'react';
 
 export const PrivateRouteAdmin = ({children, ...rest}) => {
     const {isAuthenticated} = useAuthState();
     const {getAdmin} = useContext(AuthContext);
 
-    const admin = getAdmin();
-
     return (
         <Route {...rest} render={() => {
-            return isAuthenticated && admin ? children : <Redirect to="/login"/>
+            return isAuthenticated && getAdmin() ? children : <Redirect to="/login"/>
         }}/>
     );
 }
