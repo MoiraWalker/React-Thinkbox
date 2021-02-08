d52c549a14dbf34ea7004aca33c87ddaadfe67a6
Moira <moira.walker@live.nl>
2021-02-08 18:49:21 +0100

    add project toggle

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,12 +1,14 @@
 import './index.scss';
 import {ProjectInfo} from "../../components/projectInfo";
 import {useEffect, useState} from "react";
+import {Button} from "../../components/forms/atoms/button";
 import axios from "axios";
 
 export const Projects = () => {
     const [projects, setProjects] = useState([]);
     const [isUpdated, setIsUpdated] = useState(null);
     const [isDeleted, setIsDeleted] = useState(null);
+    const [addProject, setAddProject] = useState(false);
 
     useEffect(() => {
         getAllProjects();
@@ -40,20 +42,36 @@ export const Projects = () => {
 
     console.log("projects", projects);
 
+    const addProjectForm = () => {
+        setAddProject(true);
+    }
+
     return (
         <div className='page__wrapper'>
-            <div className="page__container">
-                <h1 className="page__header">Projects</h1>
-                {projects &&
-                <div className='user__container'>
-                    {
-                        projects.map((project) => {
-                            return <ProjectInfo id={project.id} title={project.title} privateView={project.privateView} setIsDeleted={setIsDeleted} setIsUpdated={setIsUpdated}/>
-                        })
-                    }
-                </div>
-                }
-            </div>
+            {addProject ?
+                (<p>add projec</p>)
+                :
+                (<div className="page__container">
+                        <div className="page__heading">
+                            <h1 className="page__header">Projects</h1>
+                            <Button onClick={addProjectForm}>Add project</Button>
+                        </div>
+                        {projects &&
+                        <div className='user__container'>
+                            {
+                                projects.map((project) => {
+                                    return <ProjectInfo id={project.id}
+                                                        title={project.title}
+                                                        privateView={project.privateView}
+                                                        setIsDeleted={setIsDeleted}
+                                                        setIsUpdated={setIsUpdated}/>
+                                })
+                            }
+                        </div>
+                        }
+                    </div>
+                )}
+
         </div>
     );
 };
